bfaebdf197ff5ea5744133964c4d3504dadd061c
Ahmad Mesbah <Messbah.A@Gmail.com>
2023-10-13 03:18:19 +0330

    Servlet Security bug fixed

--- src/main/java/com/example/webprofile/controller/servlet/PersonServlet.java
+++ src/main/java/com/example/webprofile/controller/servlet/PersonServlet.java
@@ -1,6 +1,7 @@
 package com.example.webprofile.controller.servlet;
 
 import java.io.*;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -19,8 +20,7 @@ import jakarta.servlet.annotation.*;
 public class PersonServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-        System.out.println("GET");
+        System.out.println("Person Get");
         resp.getWriter().println("<H1>Person Servlet</H1>");
 
     }
